99220f2efed52e248376316e2a106a3211a26b4d
Mateusz Darowski <md@itdev.eu>
2020-08-12 20:36:59 +0200

    Add delete event button in example

--- example/ReactDnD11.js
+++ example/ReactDnD11.js
@@ -42,8 +42,8 @@ class ReactDnD11 extends Component {
     this.events = [
       {
         id: 2,
-        start: '2020-08-11 05:00:00',
-        end: '2020-08-11 21:00:00',
+        start: '2020-08-12 05:00:00',
+        end: '2020-08-12 21:00:00',
         resourceId: 'r3',
         title: 'Urlop',
         movable: false,
@@ -52,8 +52,8 @@ class ReactDnD11 extends Component {
       },
       {
         id: 3,
-        start: '2020-08-11 05:00:00',
-        end: '2020-08-11 21:00:00',
+        start: '2020-08-12 05:00:00',
+        end: '2020-08-12 21:00:00',
         resourceId: 'r2',
         title: 'L4',
         movable: false,
@@ -62,15 +62,15 @@ class ReactDnD11 extends Component {
       },
       {
         id: 4,
-        start: '2020-08-11 14:30:00',
-        end: '2020-08-11 16:30:00',
+        start: '2020-08-12 14:30:00',
+        end: '2020-08-12 16:30:00',
         resourceId: 'r1',
         title: 'WP',
       },
       {
         id: 5,
-        start: '2020-08-11 15:30:00',
-        end: '2020-08-11 17:30:00',
+        start: '2020-08-12 15:30:00',
+        end: '2020-08-12 17:30:00',
         resourceId: 'r2',
         title: 'LP',
         bgColor: '#f759ab'
@@ -175,6 +175,15 @@ class ReactDnD11 extends Component {
     });
   }
 
+  ops1 = (schedulerData, event) => {
+    if (event.movable === false && event.resizable === false) return false;
+
+    schedulerData.removeEvent(event);
+    this.setState({
+      viewModel: schedulerData
+    });
+  }
+
   render() {
     const {viewModel} = this.state;
 
@@ -191,6 +200,8 @@ class ReactDnD11 extends Component {
                      updateEventEnd={this.updateEventEnd}
                      moveEvent={this.moveEvent}
                      newEvent={this.newEvent}
+                     viewEventClick={this.ops1}
+                     viewEventText="Delete"
           />
         )}
       </>
